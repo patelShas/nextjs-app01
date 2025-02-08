a57f4658d52b5d8c5dba63b5538d564edc24ade8
shashwatpatel <patel.shas@northeastern.edu>
2025-02-08 10:51:26 -0500

    initial setups

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,8 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        untrained: "#a5a794",
+        neet: "#5d514f",
       },
     },
   },
